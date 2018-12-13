98b9745c24e27868ea6826abfec7eab51b0a7d62
Unknown <ivan.quevedo@uabc.edu.mx>
2018-12-13 01:57:22 -0800

    Avanze de un QR (chabelo)
    Agregue los menus para los ajustes en linea, falta editar los datos y hacer que se mantenga al perder la vision con el QR y poder salir de la app.

---
+++ New Unity Project/Assets/Scripts/Menus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menus : MonoBehaviour {
+
+	public GameObject PanelPrincipal;
+	public GameObject PanelSecundario;
+	public GameObject PanelTerceario;
+	public GameObject PanelCuarto;
+	
+	// Use this for initialization
+	public void Start () {
+		PanelTerceario.SetActive(false);	
+		PanelCuarto.SetActive(false);
+	}
+
+	public void Maestro(){
+		PanelSecundario.SetActive(false);
+		PanelTerceario.SetActive(true);	
+	}
+
+	public void Materia(){
+		PanelTerceario.SetActive(false);
+		PanelCuarto.SetActive(true);
+	}
+
+	public void Horario(){
+		Application.Quit();
+	}
+}
